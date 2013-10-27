af07b50a7ff0636ddbaa51ba8b77f01a98c4ac2e
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2013-10-27 21:41:32 +0900

    Initial import.  New files: mozc-isearch.el v0.1.0, mozc-cursor-color.el v0.1.0

---
+++ mozc-cursor-color.el
@@ -0,0 +1,112 @@
+;; mozc-cursor-color.el --- Set cursor color corresponding to mozc's input state
+
+;; Copyright (C) 2013 S. Irie
+
+;; Author: S. Irie, October 2013
+;; Keywords: mule, multilingual, input method
+
+;; Everyone is permitted to copy and distribute verbatim or modified
+;; copies of this license document, and changing it is allowed as long
+;; as the name is changed.
+
+;;            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+;;   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+;;  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+
+;;; Commentary
+
+;; This progam adds cursor color feature to mozc-mode.
+
+;;; Installation
+;;
+;; First, ensure you already can use mozc-mode.
+;;
+;; Save this file in a directory listed in load-parh, and put the
+;; following code in your .emacs file.
+;;
+;; (require 'mozc-cursor-color)
+;;
+;; Cursor color will be changed to corresponding one to state of
+;; mozc's input context.
+;;
+;; Tested on Emacs 24.
+
+
+;; History:
+;; 2013-10-27  S. Irie
+;;         * Initial release
+;;         * Version 0.1.0
+
+
+;;; Code
+
+(defvar mozc-cursor-color-conflicts-list
+  '(ac-completing))
+
+(defvar mozc-cursor-color-alist
+  '((direct . "blue")
+    (read-only . "lime green")
+    (hiragana . "red")
+    (full-katakana . "goldenrod")
+    (half-ascii . "dark orchid")
+    (full-ascii . "orchid")
+    (half-katakana . "dark goldenrod")))
+
+(defvar mozc-cursor-color-timer-delay 0.1)
+
+(defvar mozc-current-input-mode 'hiragana)
+(make-variable-buffer-local 'mozc-mode-current-mode)
+
+(defadvice mozc-session-execute-command (after mozc-current-input-mode () activate)
+  (if ad-return-value
+      (let ((mode (mozc-protobuf-get ad-return-value 'mode)))
+	(if mode
+	    (setq mozc-current-input-mode mode)))))
+
+(defvar mozc-cursor-color-timer nil)
+
+(defun mozc-cursor-color-setup-timer (&optional cancel)
+  (if (timerp mozc-cursor-color-timer)
+	(cancel-timer mozc-cursor-color-timer))
+  (setq mozc-cursor-color-timer
+	(and (not cancel)
+	     (run-with-idle-timer mozc-cursor-color-timer-delay t
+				  'mozc-cursor-color-update))))
+
+(defun mozc-cursor-color-update ()
+  (condition-case err
+      (catch 'exit
+	(mapc (lambda (symbol)
+		(if (and (boundp symbol)
+			 (symbol-value symbol))
+		    (throw 'exit nil)))
+	      mozc-cursor-color-conflicts-list)
+	(set-cursor-color
+	 (or (cdr (assq (cond
+			 ((and buffer-read-only
+			       (not inhibit-read-only))
+			  'read-only)
+			 ((not mozc-mode)
+			  'direct)
+			 (t
+			  mozc-current-input-mode))
+			mozc-cursor-color-alist))
+	     (frame-parameter nil 'foreground-color))))
+    (error
+     (mozc-cursor-color-setup-timer t)
+     (remove-hook 'post-command-hook 'mozc-cursor-color-update))))
+
+(defun mozc-cursor-color-setup ()
+  (interactive)
+  (mozc-cursor-color-setup-timer)
+  (remove-hook 'post-command-hook 'mozc-cursor-color-update)
+  (add-hook 'post-command-hook 'mozc-cursor-color-update t))
+
+(mozc-cursor-color-setup)
+
+
+(provide 'mozc-cursor-color)
+
+;;; mozc-cursor-color.el ends Here
---
+++ mozc-isearch.el
@@ -0,0 +1,115 @@
+;;; mozc-isearch.el --- Use mozc-mode in isearch-mode
+
+;; Copyright (C) 2013 S. Irie
+
+;; Author: S. Irie, October 2013
+;; Keywords: mule, multilingual, input method
+
+;; Everyone is permitted to copy and distribute verbatim or modified
+;; copies of this license document, and changing it is allowed as long
+;; as the name is changed.
+
+;;            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+;;   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+;;  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+
+;;; Commentary
+
+;; This progam allows you to use mozc-mode for the incremental search.
+
+;;; Installation
+;;
+;; First, ensure you already can use mozc-mode.
+;;
+;; Save this file in a directory listed in load-parh, and put the
+;; following code in your .emacs file.
+;;
+;; (require 'mozc-isearch)
+;;
+;; That's all.  Now you can do incremental search in Japanese.
+;;
+;; Tested on Emacs 24.
+
+
+;; History:
+;; 2013-10-27  S. Irie
+;;         * Initial release
+;;         * Version 0.1.0
+
+
+;;; Code
+
+(require 'mozc)
+
+(defvar mozc-isearch-version "0.1.0")
+
+(defvar mozc-preedit-empty-hook nil)
+(defvar mozc-mode-end-hook nil)
+
+(defadvice mozc-handle-event (around mozc-preedit-empty-hook () activate)
+  (let ((mozc-preedit-is-empty nil)) ; really ugly hack!
+    ad-do-it
+    (if mozc-preedit-is-empty
+	(run-hooks 'mozc-preedit-empty-hook))))
+
+(defadvice mozc-send-key-event (after mozc-preedit-is-empty () activate)
+  (if (and (not (and ad-return-value
+		     (mozc-protobuf-get ad-return-value 'preedit)))
+	   (boundp 'mozc-preedit-is-empty))
+      (setq mozc-preedit-is-empty t)))
+
+(defadvice mozc-mode (after mozc-mode-disabled () activate)
+  (if (not mozc-mode)
+      (run-hooks 'mozc-mode-end-hook)))
+
+(defun mozc-mode-save-minibuffer-state ()
+  (if (boundp 'mozc-mode-in-minibuffer)
+      (setq mozc-mode-in-minibuffer mozc-mode)))
+
+(defadvice isearch-process-search-multibyte-characters
+  (around mozc-isearch-process-search-characters () activate)
+  (if (and (string= current-input-method "japanese-mozc")
+	   (eq this-command 'isearch-printing-char))
+      (let ((mozc-mode-in-minibuffer mozc-mode)
+	    str)
+	(let ((overriding-terminal-local-map nil)
+	      (prompt (isearch-message-prefix))
+	      (minibuffer-local-map isearch-minibuffer-local-map)
+	      (mozc-preedit-empty-hook '(exit-minibuffer))
+	      (mozc-mode-end-hook '(exit-minibuffer))
+	      (minibuffer-exit-hook '(mozc-mode-save-minibuffer-state))
+	      junk-hist)
+	  (setq unread-command-events (cons last-char unread-command-events)
+		str (substring
+		     (read-string prompt isearch-string 'junk-hist nil t)
+		     (length isearch-string))))
+	(if (not mozc-mode-in-minibuffer)
+	    (deactivate-input-method))
+	(if (and str (> (length str) 0))
+	    (let ((unread-command-events nil))
+	      (isearch-process-search-string str str))
+	  (isearch-update)))
+    ad-do-it))
+
+(defun mozc-isearch-setup ()
+  (interactive)
+  (substitute-key-definition
+   'toggle-input-method 'isearch-toggle-input-method isearch-mode-map global-map))
+
+(add-hook 'isearch-mode-hook 'mozc-isearch-setup)
+
+
+;; Workaround for bug in mozc.el
+
+(defun mozc-mode-activate ()
+  (if (string= current-input-method "japanese-mozc")
+      (mozc-mode 1)))
+
+(add-hook 'minibuffer-setup-hook 'mozc-mode-activate)
+
+
+(provide 'mozc-isearch)
+
+;;; mozc-isearch.el ends here
